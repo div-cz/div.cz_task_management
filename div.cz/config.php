f449d52647ad9af6cc62269d709ada47d0606f68
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-03 15:33:37 +0200

    Update config.php

--- div.cz/config.php
+++ div.cz/config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "div.cz_martin";
 $password = "your_secret_password";
-$dbname = "div.cz_task_management";
+$dbname = "DIV.cz_Tasks";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
